62a56d5f49bf1ee7a02e1ca687c2b0690b8651d3
JoaoGabriel34948 <joao.freitas.oliveira27@escola.pr.gov.br>
2026-06-18 09:57:10 -0300

    Update main.js

--- main.js
+++ main.js
@@ -16,3 +16,14 @@ textos[i].classList.add("ativo")
 }
 
 const contadores=document.querySelectorAll(".contador");
+const tempoObjetivo1= new Date("2024-10-05T00:00:00");
+const tempoObjetivo2= new Date("2024-12-05T00:00:00");
+const tempoObjetivo3= new Date("2024-12-30T00:00:00);
+const tempoObjetivo4= new Date("2024-02-01T00:00:00);
+
+const tempos=[tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
+
+function calculaTempo(tempoObjetivo) {
+   let tempoAtual= new Date();
+   let tempoFinal= tempoObjetivo - tempoAtual;
+   let segundos= Math.floor(segundos/60);
